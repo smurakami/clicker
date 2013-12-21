5fe712c5d6bd1a4e5d7007688955d7bc8597edf6
s_murakami <s.murakami@conol.jp>
2013-12-21 17:32:35 +0900

    first commit

---
+++ clicker.js
@@ -0,0 +1,32 @@
+var Clicker = function(){
+    this.counter = 0;
+    this.start();
+};
+
+Clicker.prototype.start = function(){
+    var self = this;
+    this.timer = setInterval(function(){
+        self.click();
+        if(self.checkGoldenCookie()){
+            console.log('found golden cookie!');
+            self.clickGoldenCookie();
+        }
+        self.counter++;
+    }, 1);
+};
+
+Clicker.prototype.stop = function(){
+    clearInterval(this.timer);
+};
+
+Clicker.prototype.click = function(){
+    $('#bigCookie').click();
+};
+
+Clicker.prototype.checkGoldenCookie = function(){
+    return $('#goldenCookie').style.display != 'none';
+};
+
+Clicker.prototype.clickGoldenCookie = function(){
+    $('#goldenCookie').click();
+};
